7e73a92aa0f2fe41fb59bd44e7e3840c2a8191d2
flouren <bbaal@wp.pl>
2021-10-24 22:26:52 +0200

    useState and useEffect, added logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from "react";
+import {useState,useEffect} from "react";
 
 const width = 8
 const candyColours = [
@@ -13,8 +13,9 @@ const candyColours = [
 
 const App = () => {
 
-    const [currentColorArrangement,setColorArrangement] = useState()
+    const [currentColorArrangement,setCurrentColorArrangement] = useState([])
 
+    // Function that creates game board
     const createBoard = () => {
 
         const randomColorArrangement = []
@@ -24,15 +25,121 @@ const App = () => {
             randomColorArrangement.push(randomColor)
         }
         // console.log(randomColorArrangement)
-        setColorArrangement(randomColorArrangement)
-        console.log(currentColorArrangement)
+        setCurrentColorArrangement(randomColorArrangement)
     }
 
-    createBoard()
+    // Function that checks three imgs in col
+    const checkForColumnOfThree = () =>{
+      for (let i = 0; i < 47; i++) {
+        const columnOfThree = [i, i+width, i+width*2]
+        const decidedColor = currentColorArrangement[i]
+
+        if(columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+          columnOfThree.forEach(square=>currentColorArrangement[square] = '')
+        }
+    }
+  }   
+
+
+    // Function that checks four imgs in col
+    const checkForColumnOfFour = () =>{
+      for (let i = 0; i < 39; i++) {
+        const columnOfFour = [i, i+width, i+width*2,i+width*3]
+        const decidedColor = currentColorArrangement[i]
+
+        if(columnOfFour.every(square => currentColorArrangement[square] === decidedColor)){
+          columnOfFour.forEach(square=>currentColorArrangement[square] = '')
+        }
+    }
+  }   
+
+
+
+  // Function that checks if there are three imgs in row
+  const checkForRowOfThree = () =>{
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i+1, i+2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6,7,14,15,22,23,30,31,38,39,46,47,54,55,63,64]
+
+      if(notValid.includes(i)) continue
+
+      if(rowOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+        rowOfThree.forEach(square=>currentColorArrangement[square] = '')
+      }
+  }
+}  
+
+// Function that checks if there are four imgs in row
+const checkForRowOfFour = () =>{
+  for (let i = 0; i < 64; i++) {
+    const rowOfFour = [i, i+1, i+2,i+3]
+    const decidedColor = currentColorArrangement[i]
+    const notValid = [5,6,7,13,14,15,21,22,23,29,30,31,37,38,39,45,46,47,53,54,55,62,63,64]
+
+    if(notValid.includes(i)) continue
+
+    if(rowOfFour.every(square => currentColorArrangement[square] === decidedColor)){
+      rowOfFour.forEach(square=>currentColorArrangement[square] = '')
+    }
+}
+}   
+
+const moveIntoSquareBelow = () => {
+  for (let i = 0; i < 64-width; i++) {
+    if((currentColorArrangement[i + width]) === ''){
+      currentColorArrangement[i+width] = currentColorArrangement[i]
+      currentColorArrangement[i] = ''
+    }
+    
+  }
+}
+
+
+
+
+      useEffect(() => {
+        createBoard()
+        // console.log('useEffect calls');
+      },[])
+      // console.log(currentColorArrangement)
+
+
+
+
+      useEffect(() => {
+
+
+
+
+        
+        const timer = setInterval(() => {
+          checkForColumnOfFour()
+          checkForColumnOfThree()
+          checkForRowOfFour()
+          checkForRowOfThree()
+          setCurrentColorArrangement([...currentColorArrangement])
+          // console.log('Checking for threes and fours ..');
+        }, 100);
+        return ()=>clearInterval(timer)
+      }, [checkForColumnOfFour,checkForColumnOfThree,checkForRowOfThree,currentColorArrangement])
+      
+
+
+
 
   return (
     <div className="App">
-
+      <div className="game">
+        {currentColorArrangement.map((candyColours,index)=>(
+          <img
+            key={index}
+            style={{backgroundColor:candyColours}}
+            alt={candyColours}
+          >
+          </img>
+        ))}
+      </div>
     </div>
   );
 }
